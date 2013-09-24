cbc8dd8c624399e186fd4f56c770871d6ac59b96
iomz <iomz@sfc.wide.ad.jp>
2013-09-24 13:41:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transvid::Application.config.secret_key_base = 'bb8ea3281178d28426e194cc997c133a8f422e9bd151699405f8f6ac9d0a84535039a65a7b58339dbc0ba4ad55641efa430d033965b3c15f45f68f8dd3364c18'
